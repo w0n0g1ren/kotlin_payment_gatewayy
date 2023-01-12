4095cd5866e230dc313bf7e3345c02b3e986ccfb
Muhammad Nursyahid Al Faizi <nurfaizitamam@gmail.com>
2023-01-12 15:06:24 +0700

    Update Constant.kt

--- app/src/main/java/com/example/kotlin_payment_gateway/Constant.kt
+++ app/src/main/java/com/example/kotlin_payment_gateway/Constant.kt
@@ -5,10 +5,10 @@ object Constant {
     // USE YOUR OWN KEY
     // if u use this one, your app will error for sure
 
-    const val MERCHAT_ID_MIDTRANS = ""
-    const val CLIENT_KEY_MIDTRANS = "SB-Mid-client-"
-    const val SERVER_KEY_MIDTRANS = "SB-Mid-server-"
-    const val BASE_URL_MIDTRANS = ""
+   const val MERCHAT_ID_MIDTRANS = ""
+    const val CLIENT_KEY_MIDTRANS = "SB-Mid-client-WdWaBIbOfjyr1Imj"
+    const val SERVER_KEY_MIDTRANS = "SB-Mid-server-zzTtqKTEWZ47J_VuqFlMdBql"
+    const val BASE_URL_MIDTRANS = "https://midtransdicoba.000webhostapp.com/index.php/"
 
     const val KEY_ID_RAZORAPP = "rzp_test_";
     const val KEY_SECRET_RAZORAPP = "";
